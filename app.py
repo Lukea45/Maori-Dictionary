3f297d8e7490bd84b1bb147eab6b46df60094798
18039 <18039@wc.school.nz>
2022-05-05 11:11:00 +1200

    Realise my categories had to be in a table and not files, deleted files and working on table

--- app.py
+++ app.py
@@ -1,80 +1,76 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect, session
+import sqlite3
+from sqlite3 import Error
+from datetime import datetime
 app = Flask(__name__)
-
 
 @app.route('/')
 def render_homepage():
     return render_template("home.html")
 
-@app.route('/action')
-def render_action_page():
-    return render_template("action.html")
 
-@app.route('/animals')
-def render_animals_page():
-    return render_template("animals.html")
 
-@app.route('/clothing')
-def render_clothing_page():
-    return render_template("clothing.html")
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        print(request.form)
+        email = request.form.get('email')
+        password = request.form.get('password')
 
-@app.route('/culture')
-def render_culture_page():
-    return render_template("culture.html")
+        con = create_connection(DATABASE)
+        query = 'SELECT id, fname FROM users WHERE email=? AND password=?'
+        cur = con.cursor()
+        cur.execute(query, (email, password))
+        user_data = cur.fetchall()
+        con.close()
 
-@app.route('/descriptive')
-def render_descriptive_page():
-    return render_template("descriptive.html")
+        if user_data:
+            users = user_data[0][0]
+            first_name = user_data[0][1]
+            print(users, first_name)
 
-@app.route('/edibles')
-def render_edibles_page():
-    return render_template("edibles.html")
+            session['email'] = email
+            session['users'] = users
+            session['fname'] = first_name
 
-@app.route('/emotions')
-def render_emotions_page():
-    return render_template("emotions.html")
+            return redirect("/menu")
 
-@app.route('/garbage')
-def render_garbage_page():
-    return render_template("garbage.html")
+        else:
+            return redirect("/login?error=Incorrect+username+or+password")
 
-@app.route('/number')
-def render_number_page():
-    return render_template("number.html")
+    return render_template("login.html")
 
-@app.route('/outdoors')
-def render_outdoors_page():
-    return render_template("outdoors.html")
 
-@app.route('/people')
-def render_people_page():
-    return render_template("people.html")
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    if request.method == 'POST':
+        print(request.form)
+        fname = request.form.get('fname').title().strip()
+        lname = request.form.get('lname').title().strip()
+        email = request.form.get('email')
+        password = request.form.get('password')
+        password2 = request.form.get('confirm_password')
 
-@app.route('/places')
-def render_places_page():
-    return render_template("places.html")
+        if password != password2:
+            return redirect("/signup?error=Please+make+password+match")
+        print(len(password))
+        if len(password) < 8:
+            return redirect("/signup?error=Please+make+password+at+least+8+characters")
 
-@app.route('/plants')
-def render_plants_page():
-    return render_template("plants.html")
+        con = create_connection(DATABASE)
 
-@app.route('/school')
-def render_school_page():
-    return render_template("school.html")
+        query = "INSERT INTO users (fname, lname, email, password) VALUES (?, ?, ?, ?)"
+        cur = con.cursor()
+        cur.execute(query, (fname, lname, email, password))
+        con.commit()
+        con.close()
+        return redirect("/login")
 
-@app.route('/sport')
-def render_sport_page():
-    return render_template("sport.html")
+    error = request.args.get('error')
+    if error == None:
+        error = ""
 
-@app.route('/technology')
-def render_technology_page():
-    return render_template("technology.html")
-
-@app.route('/time')
-def render_time_page():
-    return render_template("time.html")
-
+    return render_template("signup.html", error=error)
 
 
 
